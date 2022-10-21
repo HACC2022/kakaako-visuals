b857b0dfdd61859eb502e4e601a9163f76e77e56
nearlyblank <nearlyblank@protonmail.com>
2022-10-20 17:34:15 -0700

    added form component, testing tailwind styling

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,11 +1,13 @@
 import {Graph} from './Graph';
 import Table from './Table';
+import Dropdown from './Dropdown';
 
 export default function Main() {
   return (
     <main className="-mt-32">
       <div className="mx-auto max-w-7xl px-4 pb-12 sm:px-6 lg:px-8">
         {/* Replace with your content */}
+        <Dropdown />
         <Graph />
         {/* /End replace */}
       </div>
--- pages/datasets/[...id].jsx
+++ pages/datasets/[...id].jsx
@@ -53,9 +53,9 @@ export default function Example() {
                     <div className="flex items-center px-2 lg:px-0">
                       <div className="flex-shrink-0">
                         <img
-                          className="block h-8 w-8"
-                          src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=300"
-                          alt="Your Company"
+                          className="block h-12 w-12"
+                          src="/seal.svg"
+                          alt="State of Hawaii Seal"
                         />
                       </div>
                       <div className="hidden lg:ml-10 lg:block">
@@ -116,58 +116,6 @@ export default function Example() {
                           />
                         )}
                       </Disclosure.Button>
-                    </div>
-                    <div className="hidden lg:ml-4 lg:block">
-                      <div className="flex items-center">
-                        <button
-                          type="button"
-                          className="flex-shrink-0 rounded-full bg-indigo-600 p-1 text-indigo-200 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-indigo-600"
-                        >
-                          <span className="sr-only">View notifications</span>
-                          <BellIcon className="h-6 w-6" aria-hidden="true" />
-                        </button>
-
-                        {/* Profile dropdown */}
-                        <Menu as="div" className="relative ml-3 flex-shrink-0">
-                          <div>
-                            <Menu.Button className="flex rounded-full bg-indigo-600 text-sm text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-indigo-600">
-                              <span className="sr-only">Open user menu</span>
-                              <img
-                                className="h-8 w-8 rounded-full"
-                                src={user.imageUrl}
-                                alt=""
-                              />
-                            </Menu.Button>
-                          </div>
-                          <Transition
-                            as={Fragment}
-                            enter="transition ease-out duration-100"
-                            enterFrom="transform opacity-0 scale-95"
-                            enterTo="transform opacity-100 scale-100"
-                            leave="transition ease-in duration-75"
-                            leaveFrom="transform opacity-100 scale-100"
-                            leaveTo="transform opacity-0 scale-95"
-                          >
-                            <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                              {userNavigation.map((item) => (
-                                <Menu.Item key={item.name}>
-                                  {({active}) => (
-                                    <a
-                                      href={item.href}
-                                      className={classNames(
-                                        active ? 'bg-gray-100' : '',
-                                        'block py-2 px-4 text-sm text-gray-700'
-                                      )}
-                                    >
-                                      {item.name}
-                                    </a>
-                                  )}
-                                </Menu.Item>
-                              ))}
-                            </Menu.Items>
-                          </Transition>
-                        </Menu>
-                      </div>
                     </div>
                   </div>
                 </div>
