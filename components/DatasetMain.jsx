66beb35af99896db22d7bd3d1eaf493d02117b8d
Chris Fryer <99569921+Frynoceros@users.noreply.github.com>
2022-10-29 12:47:03 -1000

    added full screen to loading

--- components/DatasetMain.jsx
+++ components/DatasetMain.jsx
@@ -24,52 +24,58 @@ export default function DatasetMain({datasets, setDatasets}) {
   const [startPagination, setStartPagination] = useState(0);
   const [endPagination, setEndPagination] = useState(30);
 
-  return (
-    <div className=" basis-4/5 sm:px-3 lg:px-4">
-      <div className="grid grid-cols-1 gap-4 sm:grid-cols-3">
-        {datasets
-          .map(({title, name, resources}) => (
-            <div
-              key={name}
-              className="relative flex items-center space-x-3 rounded-lg border border-gray-300 bg-white px-6 py-5 shadow-sm focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 hover:border-gray-400"
-            >
-              <div className="flex-shrink-1">
-                <Image
-                  src="/../public/seal.png"
-                  width={80}
-                  height={80}
-                  alt="Hawaii state seal"
-                />
-              </div>
-              <div className="min-w-0 flex-1 flex-wrap">
-                <Link href={`/datasets/${name}`}>
-                  <a className="focus:outline-none">
-                    <span className="absolute inset-0" aria-hidden="true" />
-                    <p className="text-sm font-medium text-gray-900">{title}</p>
-                  </a>
-                </Link>
-                <div className="flex">
-                  {resources.map(({format}) => {
+  if (!datasets) {
+    return <Loading />;
+  } else {
+    return (
+      <div className=" basis-4/5 sm:px-3 lg:px-4">
+        <div className="grid grid-cols-1 gap-4 sm:grid-cols-3">
+          {datasets
+            .map(({title, name, resources}) => (
+              <div
+                key={name}
+                className="relative flex items-center space-x-3 rounded-lg border border-gray-300 bg-white px-6 py-5 shadow-sm focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 hover:border-gray-400"
+              >
+                <div className="flex-shrink-1">
+                  <Image
+                    src="/../public/seal.png"
+                    width={80}
+                    height={80}
+                    alt="Hawaii state seal"
+                  />
+                </div>
+                <div className="min-w-0 flex-1 flex-wrap">
+                  <Link href={`/datasets/${name}`}>
+                    <a className="focus:outline-none">
+                      <span className="absolute inset-0" aria-hidden="true" />
+                      <p className="text-sm font-medium text-gray-900">
+                        {title}
+                      </p>
+                    </a>
+                  </Link>
+                  <div className="flex">
+                    {/* {resources.map(({format}) => {
                     return (
                       <>
                         <img className="h-4 w-4 m-2 " src={formatObj[format]} />
                       </>
                     );
-                  })}
+                  })} */}
+                  </div>
                 </div>
               </div>
-            </div>
-          ))
-          .slice(startPagination, endPagination)}
-      </div>
+            ))
+            .slice(startPagination, endPagination)}
+        </div>
 
-      <Pagination
-        startPagination={startPagination}
-        endPagination={endPagination}
-        setStartPagination={setStartPagination}
-        setEndPagination={setEndPagination}
-        datasets={datasets}
-      />
-    </div>
-  );
+        <Pagination
+          startPagination={startPagination}
+          endPagination={endPagination}
+          setStartPagination={setStartPagination}
+          setEndPagination={setEndPagination}
+          datasets={datasets}
+        />
+      </div>
+    );
+  }
 }
