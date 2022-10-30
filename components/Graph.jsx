81d12769dca8d662c8eb3abf68e8835c969ee026
Chris Fryer <99569921+Frynoceros@users.noreply.github.com>
2022-10-30 10:23:16 -1000

    changed text

--- components/Graph.jsx
+++ components/Graph.jsx
@@ -237,7 +237,7 @@ export default function Graph({
           className="mr-3.5 inline-flex items-center rounded-md border border-transparent bg-red-600 px-8 py-2 text-sm font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2"
           onClick={() => setMakeGraph(false)}
         >
-          Create Graph
+          Dataset Info
         </button>
       </div>
       <div>
