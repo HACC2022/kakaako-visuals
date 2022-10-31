d2755523e02ab99e6bcc9f9f0677cc4556c362c6
Chris Fryer <99569921+Frynoceros@users.noreply.github.com>
2022-10-30 20:07:14 -1000

    fixed image issue

--- components/DatasetMain.jsx
+++ components/DatasetMain.jsx
@@ -22,7 +22,7 @@ export default function DatasetMain({datasets}) {
                 className="relative flex items-center space-x-3 rounded-lg border border-gray-300 bg-white px-6 py-5 shadow-sm focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 hover:border-gray-400"
               >
                 <div className="flex-shrink-1">
-                  <Image
+                  <img
                     src="/seal.png"
                     width={80}
                     height={80}
